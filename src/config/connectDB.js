4208bc10aedb629d74be00b1f6d47763a9b2dc64
Devendra Sahu <110284377+DevendraKumarSahu09@users.noreply.github.com>
2024-08-27 19:22:03 +0530

    Update connectDB.js

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,31 +1,12 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-// const connectDB = async () => {
-//     try {
-//         const connectionInstance = await mongoose.connect(
-//             `${process.env.MONGODB_URI}/${DB_NAME}`
-//         );
-//         console.log(
-//             `\n MongoDB connected: ${connectionInstance.connection.host} \n`
-//         );
-//     } catch (error) {
-//         console.log("MongoDB connection error: ", error);
-//         process.exit(1);
-//     }
-// };
-
-// export default connectDB;
-
 const connectDB = async () => {
     try {
         const connectionString = `${process.env.MONGODB_URI}/${DB_NAME}`;
         console.log("Connecting to:", connectionString); // Add this line for debugging
 
-        const connectionInstance = await mongoose.connect(connectionString, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const connectionInstance = await mongoose.connect(connectionString);
         console.log(
             `\n MongoDB connected: ${connectionInstance.connection.host} \n`
         );
