7900a761687d186c3bdbbfcaf9deb0e0173bcc87
Devendra Sahu <110284377+DevendraKumarSahu09@users.noreply.github.com>
2024-09-18 23:21:23 +0530

    Update auth.controllers.js

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -15,7 +15,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://vidyoday-server.onrender.com/auth/google/callback",
+      callbackURL: "https://vidyoday-server.onrender.com/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       done(null, profile);
