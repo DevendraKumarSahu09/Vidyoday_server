4ad0891fa3380fdae744cb1dd7bc928d9867f3c0
Devendra Sahu <110284377+DevendraKumarSahu09@users.noreply.github.com>
2025-01-20 16:33:28 +0530

    Update auth.controllers.js

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -42,7 +42,7 @@ export const handleGoogleLoginCallback = asyncHandler(async (req, res) => {
     const jwtToken = generateJWTToken_username(existingUser);
     const expiryDate = new Date(Date.now() + 1 * 60 * 60 * 1000);
     res.cookie("accessToken", jwtToken, { httpOnly: true, expires: expiryDate, secure: false });
-    return res.redirect(`https://vidyoday-client.vercel.app/users/discover`);
+    return res.redirect(`https://vidyoday-client.vercel.app/user/discover`);
   }
 
   let unregisteredUser = await UnRegisteredUser.findOne({ email: req.user._json.email });
